cb870b979647b39e777324d692dc2327f912fa4c
Jorge Uriarte <jorge.uriarte@omelas.net>
2016-05-31 19:18:50 +0200

    Guards en inicializacion de push

--- www/js/initiatePushPlugin.js
+++ www/js/initiatePushPlugin.js
@@ -1,35 +1,37 @@
 var push
 function initiatePushPlugin() {
-    push = PushNotification.init({
-        android: {
-            senderID: 606551403580
-        },
-        ios: {
-            
-        },
-        windows: {}
-    });
+    if (typeof PushNotification !== 'undefined') {
+        push = PushNotification.init({
+            android: {
+                senderID: 606551403580
+            },
+            ios: {
+                
+            },
+            windows: {}
+        });
 
-    push.on('registration', function(data) {
-        // data.registrationId
-        //saveClientToken(data.registrationId)
-        console.log(data.registrationId)
-    });
+        push.on('registration', function(data) {
+            // data.registrationId
+            //saveClientToken(data.registrationId)
+            console.log(data.registrationId)
+        });
 
-    /*push.on('notification', function(data) {
-        // data.message,
-        // data.title,
-        // data.count,
-        // data.sound,
-        // data.image,
-        // data.additionalData
-        console.log(data.additionalData)
-        if(!data.additionalData.foreground)
-            processAutoLogin(data.additionalData.token);
-    });*/
+        /*push.on('notification', function(data) {
+            // data.message,
+            // data.title,
+            // data.count,
+            // data.sound,
+            // data.image,
+            // data.additionalData
+            console.log(data.additionalData)
+            if(!data.additionalData.foreground)
+                processAutoLogin(data.additionalData.token);
+        });*/
 
-    push.on('error', function(e) {
-        console.log(e.message)
-    });
+        push.on('error', function(e) {
+            console.log(e.message)
+        });
+    }
 }
 
