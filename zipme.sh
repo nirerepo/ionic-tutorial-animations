9ad9b27bdd28257faf69c7d56738bf7a12f3aed7
Fernando Niwes <fniwes@gmail.com>
2016-06-01 17:44:51 -0300

    Modificamos zipme.sh para compilar TypeScript antes de empaquetar el proyecto de Cordova a enviar a PhonegapBuild

--- zipme.sh
+++ zipme.sh
@@ -2,5 +2,7 @@
 #
 # To be used by Jenkins...
 #
-zip -r $1 . -x "pushTest.groovy" -x "plugins/**" -x "platforms/**" -x "node_modules/**" -x ".git/**" -x \*.ipa -x \*.xap -x \*.sh -x "**/*.sh" -x "bin/*" -x \*.apk -x "**/.svn**" -x "www/cordovareload" -x cordovareload
+npm install
+gulp compile
+zip -r $1 . -x "pushTest.groovy" -x "scss/**" -x ".git/**" -x "node-modules/**" -x "src/**" -x "plugins/**" -x "platforms/**" -x "node_modules/**" -x ".git/**" -x \*.ipa -x \*.xap -x \*.sh -x "**/*.sh" -x "bin/*" -x \*.apk -x "**/.svn**" -x "www/cordovareload" -x cordovareload
 
