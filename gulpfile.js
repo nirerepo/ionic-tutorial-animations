77e71d07a5cbaa3cc19d0824e9da2f8304b73370
Fernando Niwes <fniwes@gmail.com>
2016-06-01 15:55:20 -0300

    Configuramos gulp para utilizar Typescript. Movemos los Javascript a la carpeta src.

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,20 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var typescript = require('gulp-tsc');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  src: ['./src/*.ts']
 };
 
 gulp.task('default', ['sass']);
+
+gulp.task('compile', function() {
+  gulp.src(paths.src)
+  .pipe(typescript({ emitError: false }))
+  .pipe(gulp.dest('www/js/'))
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -29,6 +37,7 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.src, ['compile']);
 });
 
 gulp.task('install', ['git-check'], function() {
