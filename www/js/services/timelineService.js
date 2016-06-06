6168414f9eee697cea266831d2274fe3a228b618
pabloaguayo <pablo.aguayo@soluciones3f.com.ar>
2016-06-06 17:38:43 -0300

    elimino el metodo init que no se usaba

--- www/js/services/timelineService.js
+++ www/js/services/timelineService.js
@@ -1,29 +1,5 @@
 /// <reference path="../../typings/index.d.ts" />
 function TimelineService(Connection) {
-    this.init = function ($scope, $sliderDelegate) {
-        var setupSlider = function () {
-            //some options to pass to our slider
-            $scope.data.sliderOptions = {
-                initialSlide: $scope.data.currentPage,
-                direction: 'horizontal',
-                speed: 250,
-                shortSwipes: false
-            };
-            //create delegate reference to link with slider
-            $scope.data.sliderDelegate = null;
-            //watch our sliderDelegate reference, and use it when it becomes available
-            $scope.$watch('data.sliderDelegate', function (newVal, oldVal) {
-                if (newVal != null) {
-                    $scope.data.sliderDelegate.on('slideChangeEnd', function () {
-                        $scope.data.currentPage = $scope.data.sliderDelegate.activeIndex;
-                        //use $scope.$apply() to refresh any content external to the slider
-                        $scope.$apply();
-                    });
-                }
-            });
-        };
-        setupSlider();
-    };
     this.get = function () {
         var date = moment()
         return Connection.request("timeline/" + date.format("YYYYMMDD"));
