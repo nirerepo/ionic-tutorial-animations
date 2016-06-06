464eb722369be092a6f7e0fc92a1a3f893864885
pabloaguayo <pablo.aguayo@soluciones3f.com.ar>
2016-06-06 10:48:29 -0300

    Cambio la estructura del servicio

--- www/js/services/timelineService.js
+++ www/js/services/timelineService.js
@@ -1,34 +1,34 @@
 /// <reference path="../../typings/index.d.ts" />
-angular.module('starter.services')
-    .factory('Timeline', function (Connection) {
-    return {
-        init: function ($scope, $sliderDelegate) {
-            var setupSlider = function () {
-                //some options to pass to our slider
-                $scope.data.sliderOptions = {
-                    initialSlide: $scope.data.currentPage,
-                    direction: 'horizontal',
-                    speed: 250,
-                    shortSwipes: false
-                };
-                //create delegate reference to link with slider
-                $scope.data.sliderDelegate = null;
-                //watch our sliderDelegate reference, and use it when it becomes available
-                $scope.$watch('data.sliderDelegate', function (newVal, oldVal) {
-                    if (newVal != null) {
-                        $scope.data.sliderDelegate.on('slideChangeEnd', function () {
-                            $scope.data.currentPage = $scope.data.sliderDelegate.activeIndex;
-                            //use $scope.$apply() to refresh any content external to the slider
-                            $scope.$apply();
-                        });
-                    }
-                });
+function TimelineService(Connection) {
+    this.init = function ($scope, $sliderDelegate) {
+        var setupSlider = function () {
+            //some options to pass to our slider
+            $scope.data.sliderOptions = {
+                initialSlide: $scope.data.currentPage,
+                direction: 'horizontal',
+                speed: 250,
+                shortSwipes: false
             };
-            setupSlider();
-        },
-        get: function () {
-            var date = moment()
-            return Connection.request("timeline/" + date.format("YYYYMMDD"));
-        }
+            //create delegate reference to link with slider
+            $scope.data.sliderDelegate = null;
+            //watch our sliderDelegate reference, and use it when it becomes available
+            $scope.$watch('data.sliderDelegate', function (newVal, oldVal) {
+                if (newVal != null) {
+                    $scope.data.sliderDelegate.on('slideChangeEnd', function () {
+                        $scope.data.currentPage = $scope.data.sliderDelegate.activeIndex;
+                        //use $scope.$apply() to refresh any content external to the slider
+                        $scope.$apply();
+                    });
+                }
+            });
+        };
+        setupSlider();
     };
-});
+    this.get = function () {
+        var date = moment()
+        return Connection.request("timeline/" + date.format("YYYYMMDD"));
+    }
+}
+
+angular.module('starter.services')
+    .service('Timeline', TimelineService);
