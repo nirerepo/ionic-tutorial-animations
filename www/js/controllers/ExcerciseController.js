bc52dae020ab96018aec1c03af37ed8eb1cc2d65
Jorge Uriarte <jorge.uriarte@omelas.net>
2016-06-10 12:59:08 +0200

    Prueba de info adicional para tracking

--- www/js/controllers/ExcerciseController.js
+++ www/js/controllers/ExcerciseController.js
@@ -1,4 +1,4 @@
-function ExcerciseController($scope, $state, $stateParams, $ionicHistory, Exercise, Timeline){
+function ExcerciseController($scope, $state, $stateParams, $ionicHistory, $ionicModal, Exercise, Timeline){
     $scope.data = {
         "exercises": [],
         "search": ''
@@ -26,6 +26,7 @@ function ExcerciseController($scope, $state, $stateParams, $ionicHistory, Exerci
       $ionicHistory.goBack();
     };
     $scope.addExercise = function(exercise) {
+        $scope.openExerciseDetails();
         console.log(exercise.fields);
         var fields = exercise.fields;
         var exerciseData = {name: fields.nombre[0], mets: fields.mets[0], id: fields.id[0], tipo: fields.tipo[0]}
@@ -37,6 +38,34 @@ function ExcerciseController($scope, $state, $stateParams, $ionicHistory, Exerci
     $scope.getExerciseType = function(type) {
         return Exercise.type[type];
     }
+
+  $ionicModal.fromTemplateUrl('templates/exercises/track-details.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+    console.log("Asignada", $scope.modal);
+  });
+  $scope.openExerciseDetails = function() {
+    console.log("Modal...");
+    $scope.modal.show();
+    console.log("open!");
+  };
+  $scope.closeExerciseDetails = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });    
 }
 
 angular.module('starter.controllers')
