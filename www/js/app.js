3da1fca3fab1365286b80065cf29ef15099c40c7
Fernando Niwes <fniwes@gmail.com>
2016-06-14 19:21:09 -0300

    Quitamos tab-chats que no se utiliza. Unicamente se está utilizando chats-details

--- www/js/app.js
+++ www/js/app.js
@@ -85,15 +85,6 @@ angular.module('nire', [
                     }
                 }
             })
-            .state('tab.chats', {
-                url: '/chats',
-                views: {
-                    'tab-chats': {
-                        templateUrl: 'templates/tab-chats.html',
-                        controller: 'ChatsCtrl'
-                    }
-                }
-            })
             .state('tab.chat-detail', {
                 url: '/chats/:chatId',
                 views: {
