41ab5e96ca84492b8c26e9014df3dffb8b7fb813
Jorge Uriarte <jorge.uriarte@omelas.net>
2016-06-16 11:31:49 +0200

    Pruebas con Fit

--- www/js/services/HealthStoreService.js
+++ www/js/services/HealthStoreService.js
@@ -7,25 +7,33 @@ function HealthStoreService($q, $http, Connection) {
         if (navigator.health) {
             navigator.health.isAvailable(function() {
                 alert('Vamos a intentar conectar al plugin... danos permiso!');
-                var permAggregated = ['steps', 'calories', 'calories.active', 'calories.basal'];
+                var permAggregated = ['steps','calories', 'calories.active', 'calories.basal'];
                 var permUnit = ['height', 'weight', 'gender', 'date_of_birth'];
-                var successCallback = function(perm, response) {alert(perm + ": " + response.value); console.log(arguments); };
-                var errorCallback = function() { console.log("No se pudo acceder: " + arguments) };
-                console.log(permAggregated + permUnit);
-                navigator.health.requestAuthorization(permAggregated + permUnit, function() {
+                var successCallbackUnit = function(response) {
+                    console.log(response);
+                    if (response.length>0)
+                        alert(response[0].value);
+                };
+                var successCallbackAggregated = function(response) {
+                    alert(response.value);
+                };
+                var errorCallback = function() {
+                    console.log("No se pudo acceder: ", arguments);
+                };
+                navigator.health.requestAuthorization(permAggregated.concat(permUnit), function() {
                     permAggregated.forEach(function(item) {
                         navigator.health.queryAggregated({
                             startDate: new Date(new Date().getTime() - 15 * 24 * 60 * 60 * 1000), // three days ago
                             endDate: new Date(), // now
                             dataType: item
-                        }, successCallback, errorCallback)
+                        }, successCallbackAggregated, errorCallback)
                     });
                     permUnit.forEach(function(item) {
                         navigator.health.query({
                             startDate: new Date(new Date().getTime() - 15 * 24 * 60 * 60 * 1000), // three days ago
                             endDate: new Date(), // now
                             dataType: item
-                        }, successCallback, errorCallback)
+                        }, successCallbackUnit, errorCallback)
                     });
                 }, function(e) {
                     alert("No se ha obtenido autorización." + e);
