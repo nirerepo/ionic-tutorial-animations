c187b31fafacf2c5a2c5f55531694d727efa2c06
Jorge Uriarte <jorge.uriarte@omelas.net>
2016-06-21 10:21:48 +0200

    Fuera logs y traemos el campo grupo del buscador

--- www/js/services/foodService.js
+++ www/js/services/foodService.js
@@ -1,22 +1,19 @@
 function FoodService($q, $http, Connection) {
     this.plateByName = function(queryText) {
         var rdo = $q.defer();
-        var query = "http://nire0.gailen.es:9200/platos_es_co/_search?q=nombredieta:" + queryText + "&fields=id,nombredieta,kcal,cantidad,tipo_item,medida_casera,cantidad_medida_casera&sort=_score:desc";
+        var query = "http://nire0.gailen.es:9200/platos_es_co/_search?q=nombredieta:" + queryText + "&fields=id,nombredieta,kcal,cantidad,tipo_item,medida_casera,cantidad_medida_casera,grupo&sort=_score:desc";
         $http.get(query)
             .then(function (res) {
-            console.log("RES: ", res);
             rdo.resolve(res);
         }).catch(function (data, status) {
             console.error('Gists error', response.status, response.data);
         })
             .finally(function () {
-            console.log("finally finished gists");
         });
         return rdo.promise;
     };
 
     this.addPlate = function(mealId, plate, date) {
-        console.log("service.addPlate", mealId, plate, date);
         var data = { date: date, plates: [plate], idMeal: parseInt(mealId) }
         return Connection.request("track/nutrition/add", data, "application/json");
     }
