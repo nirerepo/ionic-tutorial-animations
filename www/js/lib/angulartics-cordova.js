fb428ce699d25249869887c3daa39717a6e65165
pabloaguayo <pablo.aguayo@soluciones3f.com.ar>
2016-07-18 15:03:15 -0300

    pregunto que el plugin de analytics este disponible

--- www/js/lib/angulartics-cordova.js
+++ www/js/lib/angulartics-cordova.js
@@ -1,19 +1,21 @@
 angular.module('angulartics.cordova.analytics', ['angulartics'])
   .config(['$analyticsProvider', function ($analyticsProvider) {
     $analyticsProvider.registerPageTrack(function (path) {
-        window.analytics.trackView(path, function(){console.log("track page")}, function(error){console.log("could not start analytics", error)});
+        if(window.analytics)
+            window.analytics.trackView(path, function(){console.log("track page")}, function(error){console.log("could not start analytics", error)});
     });
 
     $analyticsProvider.registerEventTrack(function (action, properties) {
-        console.log(action, properties)
-        window.analytics.trackEvent(
-            properties.eventType, 
-            action, 
-            properties.category,
-            -1,
-            function(){console.log("track event")}, 
-            function(error){console.log("could not send event", error)}
-        );
+        console.log(action, properties);
+        if(window.analytics)
+            window.analytics.trackEvent(
+                properties.eventType, 
+                action, 
+                properties.category,
+                -1,
+                function(){console.log("track event")}, 
+                function(error){console.log("could not send event", error)}
+            );
     });
 
   }])
