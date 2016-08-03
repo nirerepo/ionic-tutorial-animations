a5084c6982089b9fe8aa611764ea840058fcb45a
Jorge Uriarte <jorge.uriarte@omelas.net>
2016-08-03 02:55:58 +0200

    Hacemos las llamadas a templates *al server adecuado* :P

--- www/js/controllers/ReviewController.js
+++ www/js/controllers/ReviewController.js
@@ -1,11 +1,15 @@
-function ReviewController($scope, $stateParams, DailyReview, $ionicNavBarDelegate, $ionicHistory) {
+function ReviewController($scope, $stateParams, DailyReview, $ionicNavBarDelegate, $ionicHistory, serverConfig) {
     $scope.goBack = function() {
         $ionicHistory.goBack();
     };
 
     $scope.$on("$ionicView.beforeEnter", function () {
         DailyReview.get($stateParams.day).then(function(response) {
-            $scope.data = response.data.data; 
+            $scope.data = response.data.data;
+            console.log(serverConfig);
+            $scope.reviewHeader = serverConfig.reader + 'widgets/dailyReview.htm';
+            $scope.reviewKcal = serverConfig.reader + 'widgets/dailyCalories.htm';
+            $scope.reviewMacro= serverConfig.reader + 'widgets/dailyNutrients.htm';
         });
     });
 }
